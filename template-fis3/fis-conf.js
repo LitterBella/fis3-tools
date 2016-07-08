1190d030369a897b88a606a21684f3c6ba59f06d
fisker <lionkay@gmail.com>
2016-07-08 11:26:45 +0800

    update js lint to fis3-lint-eslint-noisy

--- template-fis3/fis-conf.js
+++ template-fis3/fis-conf.js
@@ -189,7 +189,10 @@ last update 2016.1.7
     'fis-parser-jade-to-html': {
       pretty: true,
     },
-    'fis3-lint-htmlhint': {}
+    'fis3-lint-htmlhint': {},
+    'fis3-lint-eslint-noisy': {
+      useEslintrc: true
+    }
   };
 
   var pluginTypes = [
@@ -255,8 +258,7 @@ last update 2016.1.7
     },
     {
       type: 'js',
-      // fis-lint-jscs has a bug can't use
-      lint: CONFIG.LINT.JS ? ['fis-lint-jshint', 'fis-lint-eslint'] : null,
+      lint: CONFIG.LINT.JS ? 'fis3-lint-eslint-noisy' : null,
       optimizer: CONFIG.OPTIMIZER.JS ? 'fis-optimizer-uglify-js' : null,
     },
     {
@@ -452,6 +454,7 @@ last update 2016.1.7
     var processor = {};
 
     // lint can't used on preProcessor
+    // and we only lint for prodution
     if (data.lint) {
       $.match(getExtsReg(toArray(data.type)), {
         lint: getPlugin(data.lint)
@@ -513,81 +516,5 @@ last update 2016.1.7
     .match('**', {
       optimizer: null
     });
-
-  // fis-lint-eslint requires Array.prototype.fill
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/fill
-  /* eslint-disable */
-  /* jshint ignore:start */
-  /* jscs: disable */
-  if (!Array.prototype.fill) {
-    Array.prototype.fill = function(value) {
-
-      // Steps 1-2.
-      if (this == null) {
-        throw new TypeError('this is null or not defined');
-      }
-
-      var O = Object(this);
-
-      // Steps 3-5.
-      var len = O.length >>> 0;
-
-      // Steps 6-7.
-      var start = arguments[1];
-      var relativeStart = start >> 0;
-
-      // Step 8.
-      var k = relativeStart < 0 ?
-        Math.max(len + relativeStart, 0) :
-        Math.min(relativeStart, len);
-
-      // Steps 9-10.
-      var end = arguments[2];
-      var relativeEnd = end === undefined ?
-        len : end >> 0;
-
-      // Step 11.
-      var final = relativeEnd < 0 ?
-        Math.max(len + relativeEnd, 0) :
-        Math.min(relativeEnd, len);
-
-      // Step 12.
-      while (k < final) {
-        O[k] = value;
-        k++;
-      }
-
-      // Step 13.
-      return O;
-    };
-  }
-  /* jscs: enable */
-  /* jshint ignore:end */
-  /* eslint-enable */
-
-  // function readConfig(file) {
-  //   var fs = require('fs');
-  //   var path = require("path");
-  //   var currentFolder = process.cwd();
-  //   var filename;
-  //   var parentFolder;
-
-  //   while(true) {
-  //     filename = path.normalize(path.join(currentFolder, file));
-  //     if (fs.existsSync(filename)) {
-  //       try {
-  //         return JSON.parse(require('fs').readFileSync(filename, 'utf8'));
-  //       }catch(_){
-  //         return;
-  //       }
-  //     }
-
-  //     parentFolder = path.resolve(currentFolder, '../');
-  //     if (parentFolder === currentFolder) {
-  //       return;
-  //     }
-  //     currentFolder = parentFolder;
-  //   }
-  // }
 
 })(fis);
