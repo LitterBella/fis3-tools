b45b66bb3aa109eeb8aab37a874963f9e82dd353
fisker <lionkay@gmail.com>
2016-09-27 17:04:49 +0800

    fix meter

---
+++ template-fis3/source/static/js/component/_meter.js
@@ -0,0 +1,52 @@
+  function initMeter() {
+    var meter = $(this);
+    if (meter.data('inited')) {
+      return;
+    }
+    meter.data('inited', true);
+    $('<div class="meter-bar">').appendTo(meter);
+    var value = +meter.val() || +meter.attr('value');
+    console.log(value);
+    setMeterValue(meter, value);
+  }
+
+  function setMeterValue(meter, value) {
+    meter = $(meter).val(value);
+    var max = +meter.attr('max') || 100;
+    var meterBar = meter.find('.meter-bar');
+
+    var low = +meter.attr('low');
+    var high = +meter.attr('high');
+    var optimum = +meter.attr('optimum');
+    var isHigerBetter = true;
+    if (optimum && optimum <= low) {
+      isHigerBetter = false;
+    }
+
+    var className = 'meter-sub-optimum-value';
+    if (isHigerBetter) {
+      if (value <= low) {
+        className = 'meter-sub-sub-optimum-value';
+      } else if (value >= high) {
+        className = 'meter-optimum-value';
+      }
+    } else {
+      if (value >= high) {
+        className = 'meter-sub-sub-optimum-value';
+      } else if (value <= low) {
+        className = 'meter-optimum-value';
+      }
+    }
+
+    meterBar.css('width', value / max * 100 + '%')[0].className = 'meter-bar ' + className;
+  }
+
+  $.fn.meterValue = $.fn.meterVal = function(value) {
+    return $(this).each(function() {
+      setMeterValue(this, value);
+    });
+  };
+
+  $(function() {
+    $('meter').each(initMeter);
+  });
--- template-fis3/source/static/js/main.js
+++ template-fis3/source/static/js/main.js
@@ -4,6 +4,7 @@
   // __inline('component/_pseudo.js')
   // __inline('component/_date-placeholder.js')
   // __inline('component/_number-input.js')
+  // __inline('component/_meter.js')
 
 
 })();
